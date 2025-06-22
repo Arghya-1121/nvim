24bbdc505561ce2fa69433a7a4d8c18a80c13427
Arghya Sarkar <arghya.sarkar@yandex.com>
2025-06-22 20:21:34 +0530

    reduce storage dependancy

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.o.directory = "/tmp/"
+vim.o.undodir = "/tmp/"
+vim.o.backupdir = "/tmp/"
+vim.env.XDG_CACHE_HOME = "/tmp"
