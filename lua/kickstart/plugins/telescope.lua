f667f083560fcaff9304a4d525991d750d78e145
Arghya Sarkar <arghya1121@gmail.com>
2025-11-16 01:37:41 +0530

    minimal

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -69,7 +69,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>o', builtin.buffers, { desc = 'Show open buffers' })
     vim.keymap.set('n', '<leader>sq', ':bp | bd #<CR>', { desc = '[S]earch [Q]uit/Delete buffer (keep split)' })
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>s?', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
